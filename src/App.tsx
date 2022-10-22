f56383271e5e3a854120e1482e2520323b3584bc
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2022-10-22 18:24:08 +0600

    Level 1 done
    Modified CardView.tsx, App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import CardView from "./CardView/CardView";
 function App() {
   const [cards, setCards] = useState([
     {rank: '3', suit:'spades'},
-    {rank: 'j', suit:'hearts'},
-    {rank: 'a', suit:'clubs'},
+    {rank: 'J', suit:'hearts'},
+    {rank: 'A', suit:'clubs'},
     {rank: '10', suit:'diams'},
-    {rank: 'j', suit:'spades'},
+    {rank: 'J', suit:'spades'},
   ])
 
   return (
     <div className="App">
       <div className="playingCards faceImages">
         <CardView rank={cards[0].rank} suit={cards[0].suit}/>
+        <CardView rank={cards[1].rank} suit={cards[1].suit}/>
+        <CardView rank={cards[2].rank} suit={cards[2].suit}/>
+        <CardView rank={cards[3].rank} suit={cards[3].suit}/>
+        <CardView rank={cards[4].rank} suit={cards[4].suit}/>
       </div>
     </div>
   );
--- src/CardView/CardView.tsx
+++ src/CardView/CardView.tsx
@@ -5,19 +5,25 @@ interface CardProps extends  React.PropsWithChildren {
   suit: string;
 }
 
-
-
 const CardView: React.FC<CardProps> = (props) => {
-  const cardClass = 'card rank-'+props.rank + ' ' +props.suit
+  const cardClass = 'card rank-'+props.rank.toLowerCase() + ' ' +props.suit
+  let symbol;
+  if (props.suit === 'clubs'){
+    symbol = '♣';
+  } else if (props.suit === 'spades'){
+    symbol = '♠';
+  } else if (props.suit === 'diams'){
+    symbol = '♦';
+  }else {
+    symbol = '♥';
+  }
 
   return (
     <span className={cardClass}>
       <span className="rank">{props.rank}</span>
-      <span className="suit"></span>
-    <span>{props.children}</span>
-</span>
-
-
+      <span className="suit">{symbol}</span>
+      <span>{props.children}</span>
+    </span>
   );
 };
 
